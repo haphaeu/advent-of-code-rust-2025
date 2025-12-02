824acdfcfcd70e464009d0fd1b71c4284e8d7a33
Rafael Rossi <rossirafael@yahoo.com>
2025-12-02 15:05:54 +0100

    day 2 done

--- README.md
+++ README.md
@@ -18,9 +18,9 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
 | [Day 1](./src/bin/01.rs) | `103.4µs` | `129.3µs` |
-| [Day 2](./src/bin/02.rs) | `-` | `-` |
+| [Day 2](./src/bin/02.rs) | `104.8ms` | `417.2ms` |
 
-**Total: 0.23ms**
+**Total: 522.23ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/02.rs
+++ src/bin/02.rs
@@ -7,12 +7,102 @@ fn parse_input(input: &str) -> Vec<Vec<&str>> {
         .collect()
 }
 
+fn check_part_one(num: u64) -> bool {
+    let s = num.to_string();
+    if s.len() % 2 != 0 {
+        return false;
+    }
+    s[0..(s.len()/2)] == s[(s.len()/2)..s.len()]
+
+}
+
+fn check_part_two(num: u64) -> bool {
+    let s = num.to_string();
+
+    if s.len() == 1 {
+        return false;
+    }
+
+    // 1. all digits are the same
+    if s.chars().next().map(|f| s.chars().all(|c| c == f)).unwrap_or(false) {
+        return true;
+    }
+
+    // 2. pairs of digits
+    if s.len() < 4 {
+        return false;
+    }
+
+    let chars = s.chars().collect::<Vec<char>>();
+    let pair = [chars[0], chars[1]];
+    if chars.chunks(2).all(|chunk| chunk == pair) {
+        return true;
+    }
+
+    // 3. triplets of digits
+    if s.len() < 6 {
+        return false;
+    }
+    let triplet = [chars[0], chars[1], chars[2]];
+    if chars.chunks(3).all(|chunk| chunk == triplet) {
+        return true;
+    }
+
+    // 4. quadruplets of digits
+    if s.len() < 8 {
+        return false;
+    }
+    let quadruplet = [chars[0], chars[1], chars[2], chars[3]];
+    if chars.chunks(4).all(|chunk| chunk == quadruplet) {
+        return true;
+    }
+
+    // 5. quintuplets of digits
+    if s.len() < 10 {
+        return false;
+    }
+    let quintuplet = [chars[0], chars[1], chars[2], chars[3], chars[4]];
+    if chars.chunks(5).all(|chunk| chunk == quintuplet) {
+        return true;
+    }
+    false
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+
+    let ranges = parse_input(input);
+    let mut sum = 0;
+
+    for range in ranges {
+        let start = range[0];
+        let end = range[1];
+        let start_num: u64 = start.parse().unwrap();
+        let end_num: u64 = end.parse().unwrap();
+        for num in start_num..=end_num {
+            if check_part_one(num) {
+                sum += num;
+            }
+        }
+    }
+    Some(sum)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let ranges = parse_input(input);
+    let mut sum = 0;
+
+    for range in ranges {
+        let start = range[0];
+        let end = range[1];
+        let start_num: u64 = start.parse().unwrap();
+        let end_num: u64 = end.parse().unwrap();
+        for num in start_num..=end_num {
+            if check_part_two(num) {
+                sum += num;
+            }
+        }
+    }
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -22,12 +112,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(1227775554));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4174379265));
     }
 }
